0844fe84334bbc142fc8ab237c86be5aa9da3891
Chinaemerem <alionyechinaemerem@gmail.com>
2022-06-29 22:23:49 +0100

    project_update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,18 +6,18 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp1 = NULL, *temp2 = NULL;
+listint_t *temp1 = NULL, *temp2 = NULL;
 
-	temp1 = list;
-	temp2 = list;
+temp1 = list;
+temp2 = list;
 
-	while (list)
-	{
-		temp2 = temp2->next;
-		if (!temp1 || !temp2)
-			return (0);
-		if (temp2 == temp1)
-			return (1);
-	}
-	return (0);
+while (list)
+{
+temp2 = temp2->next;
+if (!temp1 || !temp2)
+return (0);
+if (temp2 == temp1)
+return (1);
+}
+return (0);
 }
