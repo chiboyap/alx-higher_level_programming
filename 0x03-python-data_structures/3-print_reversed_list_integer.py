d7d420121f722b20096be4a17bd1bcde0d70362b
chiboyap <alionyechinaemerem@gmail.com>
2022-07-04 09:22:28 +0100

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=None):
     """
     A function that prints all integers of a list, in reverse order
     """
+    if my_list is None:
+        my_list = []
     for i in my_list[::-1]:
         print("{:d}".format(i))
